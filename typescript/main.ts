cb084b951ddb83c404e39ba3da3866915b022e13
BigmacMarc <stesl.noah06@gmail.com>
2026-01-19 10:16:35 +0100

    game

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,5 +1,8 @@
-import { Car } from "./car.ts";
+import { Car } from "./car";
+import { Engine } from "./engine";
 
 const myCar = new Car("red", 2020);
+const engine = new Engine(150, "Diesel");
 
 console.log(myCar.getColor());
+console.log(engine.getHorsepower());
